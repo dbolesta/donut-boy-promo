eb25392287dfe0a166e0e246dbcb675e40c8fd25
Damon Bolesta <dbolesta@gmail.com>
2019-08-28 19:36:53 -0400

    adds ability to mute audio player /w memory by clicking speaker icon

--- src/js/musicPlayer.js
+++ src/js/musicPlayer.js
@@ -1,19 +1,24 @@
-var audio = $(".player").find("audio")[0];
+var audio = $('.player').find('audio')[0];
 
 $(document).ready(function() {
-  var player = $(".player");
-  var _button = $(player).find(".btn-music");
+  var player = $('.player');
+  var _button = $(player).find('.btn-music');
 
   // find the audio tag
   //var audio = $(player).find("audio")[2];
-  var seekbarInner = $(player).find(".seek-bar .inner");
-  var seekbarOuter = $(player).find(".seek-bar .outer");
-  var volumeControl = $(player).find(".volume-control .volume-bar");
+  var seekbarInner = $(player).find('.seek-bar .inner');
+  var seekbarOuter = $(player).find('.seek-bar .outer');
+  var volumeControl = $(player).find('.volume-control .volume-bar');
+  var volumeIcon = $(player).find('.volume-control .volume-icon');
 
   var length;
   var interval; // used to run setInterval Function
   var seekbarPercentage;
-  var volumePercentage;
+  var muteVolume; // used to remember where volume was before muting
+  var mutePosition; // same but for volume handle and inner bar
+
+  var volumePosition; // position of volume handle & inner bar
+  var audioVolume; // volume of audio
 
   var seekDrag = false; // for dragging seek position
   var volumeDrag = false;
@@ -23,20 +28,20 @@ $(document).ready(function() {
   // seekbar control
 
   $(document).on(
-    "mousedown",
-    ".seek-bar .outer, .seek-handle",
+    'mousedown',
+    '.seek-bar .outer, .seek-handle',
     function(e) {
       seekDrag = true;
       updateSeekbar(e);
     }
   );
-  $(document).on("mouseup", function(e) {
+  $(document).on('mouseup', function(e) {
     if (seekDrag) {
       seekDrag = false;
       updateSeekbar(e);
     }
   });
-  $(document).on("mousemove", function(e) {
+  $(document).on('mousemove', function(e) {
     if (seekDrag) {
       updateSeekbar(e);
     }
@@ -44,58 +49,68 @@ $(document).ready(function() {
 
   // volume control
   $(document).on(
-    "mousedown",
-    ".volume-control .outer, .volume-handle",
+    'mousedown',
+    '.volume-control .outer, .volume-handle',
     function(e) {
       volumeDrag = true;
       updateVolume(e);
     }
   );
-  $(document).on("mouseup", function(e) {
+  $(document).on('mouseup', function(e) {
     if (volumeDrag) {
       volumeDrag = false;
       updateVolume(e);
     }
   });
-  $(document).on("mousemove", function(e) {
+  $(document).on('mousemove', function(e) {
     if (volumeDrag) {
       updateVolume(e);
     }
   });
+  $(volumeIcon).on('click', function(e) {
+    toggleVolume(e);
+  });
 
-  var updateVolume = function(e) {
-    var volumePosition = e.pageX - volumeControl.offset().left;
-    var audioVolume = volumePosition / volumeControl.width();
+  var updateVolume = function(e, position, volume) {
+    // assign position of volume thing & audio either my calculating, or if a param is passed
+    volumePosition =
+      position != undefined
+        ? position
+        : e.pageX - volumeControl.offset().left;
+    audioVolume =
+      volume != undefined
+        ? volume
+        : volumePosition / volumeControl.width();
 
     if (audioVolume >= 0 && audioVolume <= 1) {
       audio.volume = audioVolume;
       volumeControl
-        .find(".inner")
-        .css("width", audioVolume * 100 + "%");
-      $(".volume-bar .volume-handle").css(
-        "left",
-        audioVolume * 100 + "%"
+        .find('.inner')
+        .css('width', audioVolume * 100 + '%');
+      $('.volume-bar .volume-handle').css(
+        'left',
+        audioVolume * 100 + '%'
       );
     }
   };
 
-  // click for volume control
-  /*volumeControl.find(".outer").on('click', function(e){
+  var toggleVolume = function(e) {
+    if (audio.volume != 0) {
+      // remember what the values were before muting
+      mutePosition = volumePosition;
+      muteVolume = audioVolume;
+      // mute
+      updateVolume(null, 0, 0);
+    } else {
+      // unmute
+      updateVolume(null, mutePosition, muteVolume);
+    }
+  };
 
-            var volumePosition = e.pageX - $(this).offset().left;
-            var audioVolume = volumePosition / $(this).width();
+  $('.btn-forward').on('click', function() {
+    var numOfSongs = $('.player audio').legnth;
 
-            if(audioVolume >= 0 && audioVolume <= 1){
-                audio.volume = audioVolume;
-                $(this).find(".inner").css("width", audioVolume * 100 + "%");
-
-            }
-        });*/
-
-  $(".btn-forward").on("click", function() {
-    var numOfSongs = $(".player audio").legnth;
-
-    $(".player audio").each(function(index, value) {
+    $('.player audio').each(function(index, value) {
       console.log(index, value);
 
       if (audio == value) {
@@ -112,10 +127,10 @@ $(document).ready(function() {
     });
   });
 
-  $(".btn-back").on("click", function() {
-    var numOfSongs = $(".player audio").legnth;
+  $('.btn-back').on('click', function() {
+    var numOfSongs = $('.player audio').legnth;
 
-    $(".player audio").each(function(index, value) {
+    $('.player audio').each(function(index, value) {
       console.log(index, value);
 
       if (audio == value) {
@@ -136,25 +151,25 @@ $(document).ready(function() {
 
   var playButton = function() {
     // check for play class
-    if (_button.hasClass("play")) {
-      _button.removeClass("play").addClass("pause");
+    if (_button.hasClass('play')) {
+      _button.removeClass('play').addClass('pause');
 
       // find length of audio
       length = audio.duration;
       // set end time
       $(player)
-        .find(".timing .end")
+        .find('.timing .end')
         .text(sToTime(length));
 
       // play the audio
-      console.log("Audio is playing!!!");
+      console.log('Audio is playing!!!');
       audio.play();
 
       // interval stuff
       intervalCheck();
     } // end if loop
-    else if (_button.hasClass("pause")) {
-      _button.removeClass("pause").addClass("play");
+    else if (_button.hasClass('pause')) {
+      _button.removeClass('pause').addClass('play');
       clearInterval(interval);
       audio.pause();
     } // end else
@@ -164,13 +179,13 @@ $(document).ready(function() {
     //$(".track-list li.selected i").toggleClass("animate");
   }; // end playButton()
 
-  _button.on("click", function() {
+  _button.on('click', function() {
     playButton();
   });
 
   var intervalCheck = function() {
     interval = setInterval(function() {
-      console.log("setinterval is running");
+      console.log('setinterval is running');
       // update seek bar
       if (!audio.paused) {
         // while audio is playing
@@ -180,17 +195,17 @@ $(document).ready(function() {
       //Audio has ended
       if (audio.ended) {
         clearInterval(interval);
-        _button.removeClass("pause").addClass("play");
-        seekbarInner.width(100 + "%");
+        _button.removeClass('pause').addClass('play');
+        seekbarInner.width(100 + '%');
         updateMusicAnim();
       }
     }, 250);
   };
 
   var updateMusicAnim = function() {
-    $(".track-list li i").removeClass("animate");
+    $('.track-list li i').removeClass('animate');
     if (!audio.paused) {
-      $(".track-list li.selected i").addClass("animate");
+      $('.track-list li.selected i').addClass('animate');
     }
   };
   var updateSeekbarInterval = function() {
@@ -200,19 +215,19 @@ $(document).ready(function() {
     );
 
     // update seekbar percentage
-    $(seekbarInner).css("width", seekbarPercentage + "%");
-    $(".seek-bar .seek-handle").css("left", seekbarPercentage + "%");
+    $(seekbarInner).css('width', seekbarPercentage + '%');
+    $('.seek-bar .seek-handle').css('left', seekbarPercentage + '%');
 
     // update current/start time
     $(player)
-      .find(".timing .start")
+      .find('.timing .start')
       .text(sToTime(audio.currentTime));
   }; // end updateSeekbar1()
 
   //update seekbar
   var updateSeekbar = function(e) {
     if (/*!audio.ended && */ length !== undefined) {
-      console.log("this is e: ", e);
+      console.log('this is e: ', e);
       var seekPosition = e.pageX - $(seekbarOuter).offset().left;
 
       if (
@@ -228,15 +243,15 @@ $(document).ready(function() {
         );
 
         // update seekbar percentage
-        $(seekbarInner).css("width", seekbarPercentage + "%");
-        $(".seek-bar .seek-handle").css(
-          "left",
-          seekbarPercentage + "%"
+        $(seekbarInner).css('width', seekbarPercentage + '%');
+        $('.seek-bar .seek-handle').css(
+          'left',
+          seekbarPercentage + '%'
         );
 
         // update current/start time
         $(player)
-          .find(".timing .start")
+          .find('.timing .start')
           .text(sToTime(audio.currentTime));
       }
     }
@@ -248,10 +263,10 @@ $(document).ready(function() {
     return parseFloat(calcPercentage.toString());
   };
 
-  $(".track-list li").on("click", function() {
+  $('.track-list li').on('click', function() {
     var index = $(this).index();
 
-    if (audio != $(player).find("audio")[index]) {
+    if (audio != $(player).find('audio')[index]) {
       changeSong(index);
     }
   });
@@ -265,32 +280,32 @@ $(document).ready(function() {
     }
 
     // update audio var to correct song, also capture length of new song
-    audio = $(".player").find("audio")[index];
+    audio = $('.player').find('audio')[index];
     length = audio.duration;
 
     // update length of song on track
     $(player)
-      .find(".timing .end")
+      .find('.timing .end')
       .text(sToTime(length));
 
     // remove icon next to song, add to correct song via index using eq()
-    $(".track-list li").removeClass("selected");
-    $(".track-list li")
+    $('.track-list li').removeClass('selected');
+    $('.track-list li')
       .eq(index)
-      .addClass("selected");
+      .addClass('selected');
     // note for future damon:
     // eq(): Reduce the set of matched elements to the one at the specified index.
     // https://api.jquery.com/eq/
 
-    if (_button.hasClass("play")) {
-      _button.removeClass("play").addClass("pause");
+    if (_button.hasClass('play')) {
+      _button.removeClass('play').addClass('pause');
       intervalCheck();
     }
 
     // reset time to beginning
     audio.currentTime = 0;
     // set volume to whatever volume bar is set at
-    audio.volume = $(".volume-bar .inner").width() / 100;
+    audio.volume = $('.volume-bar .inner').width() / 100;
     audio.play();
 
     updateMusicAnim();
@@ -298,16 +313,16 @@ $(document).ready(function() {
     // capture track name from track list using regex, and put it in main player
     // \D+         <-- capture all nondigit characters...
     // [^\s\d+]    <-- ... until we find a single space followed by 1 or more digits (track time)
-    var name = $(".track-list li")[index].textContent.match(
+    var name = $('.track-list li')[index].textContent.match(
       /\D+[^\s\d+]/
     )[0];
-    $(".audio-name")[0].textContent = name;
+    $('.audio-name')[0].textContent = name;
   }; // end changeSong()
 }); // end document ready!
 
 function sToTime(t) {
-  return parseInt((t / 60) % 60) + ":" + padZero(parseInt(t % 60));
+  return parseInt((t / 60) % 60) + ':' + padZero(parseInt(t % 60));
 }
 function padZero(v) {
-  return v < 10 ? "0" + v : v;
+  return v < 10 ? '0' + v : v;
 }
